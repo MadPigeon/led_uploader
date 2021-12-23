b7d28a6d6090ecb18196fb48ed42c249357957d8
Vsevolod Rychkov <v.rychkov@brusnika.ru>
2021-12-23 17:21:12 +0500

    started testing file_id extraction, added examples of messages

--- __tests__/classes/FileExtracter.test.ts
+++ __tests__/classes/FileExtracter.test.ts
@@ -1,5 +1,34 @@
-import { extractPhoto } from '../../src/classes/FileExtracter';
+import { extractBestQualityPhoto } from '../../src/classes/FileExtracter';
+
+test('extractPhoto exists', () => {
+    expect(typeof extractBestQualityPhoto === 'function').toEqual(true);
+});
 
 test('function exists', () => {
-  expect(typeof extractPhoto === 'function').toEqual(true);
+    let input: { message_id: number; photo: { file_id: string; file_size: number; width: number; height: number; }[]; };
+    input = {
+        message_id: 75,
+        photo: [
+            {
+              "file_id": "AgACAgIAAxkBAANLYcRcIMpbCeT1HCL497UAAUatikM9AAKcuTEbM2MgSq1mSlq1OoDuAQADAgADcwADIwQ",
+              "file_size": 718,
+              "width": 90,
+              "height": 67
+            },
+            {
+              "file_id": "AgACAgIAAxkBAANLYcRcIMpbCeT1HCL497UAAUatikM9AAKcuTEbM2MgSq1mSlq1OoDuAQADAgADeAADIwQ", 
+              "file_size": 4199,
+              "width": 400,
+              "height": 300
+            },
+            {
+              "file_id": "AgACAgIAAxkBAANLYcRcIMpbCeT1HCL497UAAUatikM9AAKcuTEbM2MgSq1mSlq1OoDuAQADAgADbQADIwQ", 
+              "file_size": 4303,
+              "width": 320,
+              "height": 240
+            }
+          ]
+    };
+    const expected = "AgACAgIAAxkBAANLYcRcIMpbCeT1HCL497UAAUatikM9AAKcuTEbM2MgSq1mSlq1OoDuAQADAgADeAADIwQ";
+    expect(extractBestQualityPhoto(input)).toEqual(expected);
 });
--- src/classes/FileExtracter.ts
+++ src/classes/FileExtracter.ts
@@ -1,3 +1,8 @@
-export function extractPhoto() {
+export function extractBestQualityPhoto(message: { message_id: number; photo: { file_id: string; file_size: number; width: number; height: number; }[]; }) {
+    const sorted_photos = message.photo.sort(compareByWidthDescending());
+    return sorted_photos[0].file_id;
+}
 
+function compareByWidthDescending(): (photo1: { width: number; }, photo2: { width: number; }) => number {
+    return (photo1, photo2) => photo2.width - photo1.width;
 }
