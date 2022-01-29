8b33cb84c0796f4103b908e110a05d66aeba258a
MadPigeon <vsevolod007@gmail.com>
2022-01-29 16:09:01 +0500

    added python code and test suite for it

---
+++ python/test_agent.py
@@ -0,0 +1,10 @@
+import unittest
+
+from python.src.agent import Agent
+
+
+class TestAgent(unittest.TestCase):
+
+    def test_agent_created(self):
+        test = Agent
+        self.assertEqual(0, 0)
