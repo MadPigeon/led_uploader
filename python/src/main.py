d1caf2be23d03404ab49878841416cb48ff8e5ff
MadPigeon <vsevolod007@gmail.com>
2022-02-09 01:24:24 +0500

    only takes forwarded messages from the specific chat

--- python/src/main.py
+++ python/src/main.py
@@ -11,12 +11,12 @@ def print_text(name, text):
     print(name + " said: " + text)
 
 
-def register(cb):
+def register(cb, the_chat_id):
     # `pattern` is a regex, see https://docs.python.org/3/library/re.html
     # Use https://regexone.com/ if you want a more interactive way of learning.
     #
     # "(?i)" makes it case-insensitive, and | separates "options".
-    @client.on(events.NewMessage(pattern=r'(?i).*\b(hello|hi)\b'))
+    @client.on(events.NewMessage(chats=the_chat_id, forwards=True))
     async def handler(event):
         sender = await event.get_sender()
         name = utils.get_display_name(sender)
@@ -39,11 +39,12 @@ agent_config = json.load(config_file)
 session = agent_config["session"]
 api_id = agent_config["api_id"]
 api_hash = agent_config["api_hash"]
+the_chat_id = agent_config["chat_id"]
 
 proxy = None  # https://github.com/Anorov/PySocks
 
 client = TelegramClient(session, api_id, api_hash, proxy=proxy).start()
 
-register(print_text)
+register(print_text, the_chat_id)
 
 run()
