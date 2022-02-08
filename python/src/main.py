84bbb6829f7c7fb93429baeccd63a197370333f1
MadPigeon <vsevolod007@gmail.com>
2022-02-09 02:57:54 +0500

    agent now saves file using timestamp inside target folder

--- python/src/main.py
+++ python/src/main.py
@@ -1,26 +1,29 @@
 import json
+import time
 
 from telethon import TelegramClient, events, utils
 from agent import Agent
 
 
-def print_text(name, text):
-    print(name + " said: " + text)
+def print_text(file_name):
+
+    print(file_name + " was saved.")
 
 
-def register(cb, the_chat_id):
+def register(cb, the_chat_id, folder):
     # `pattern` is a regex, see https://docs.python.org/3/library/re.html
     # Use https://regexone.com/ if you want a more interactive way of learning.
     #
     # "(?i)" makes it case-insensitive, and | separates "options".
     @client.on(events.NewMessage(chats=the_chat_id, forwards=True))
     async def handler(event):
+        message = event.message
+        new_name = folder + '/' + format(int(round(time.time())), 'd')
+        await message.download_media(new_name)
+
         sender = await event.get_sender()
         name = utils.get_display_name(sender)
-        message = event.message
-        print(message)
-        await message.download_media('./download/')
-        cb(name, event.text)
+        cb(new_name)
 
 
 def run():
@@ -32,19 +35,23 @@ def run():
 
 
 test = Agent()
-config_file = open('../../config/agent_config.json', encoding='UTF8')
+config_file_agent = open('../../config/agent_config.json', encoding='UTF8')
 
-agent_config = json.load(config_file)
+agent_config = json.load(config_file_agent)
 
 session = agent_config["session"]
 api_id = agent_config["api_id"]
 api_hash = agent_config["api_hash"]
 the_chat_id = agent_config["chat_id"]
 
+config_file_app = open('../../config/app_config.json', encoding='UTF8')
+app_config = json.load(config_file_app)
+folder = app_config["folder"]
+
 proxy = None  # https://github.com/Anorov/PySocks
 
 client = TelegramClient(session, api_id, api_hash, proxy=proxy).start()
 
-register(print_text, the_chat_id)
+register(print_text, the_chat_id, folder)
 
 run()
